5226734126c80d8afbd1b34bc6b542202a68c2a7
linqi <lin@linqideMac-mini.local>
2020-01-28 14:47:55 +0800

    immutable和immutable-redux的引入

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -50,7 +50,7 @@ const Header = props => {
 
 const mapStateToProps = state => {
   return {
-    focused: state.header.get('focused')
+    focused: state.getIn(['header'],['focused'])
   };
 };
 const mapDispatchToProps = dispatch => {
