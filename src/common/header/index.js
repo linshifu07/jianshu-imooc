072106b6fe0f94bbf1c2535f7e42ec5458d419d4
linqi <lin@linqideMac-mini.local>
2020-01-28 22:26:47 +0800

    热门搜索换一换功能实现

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -48,9 +48,7 @@ class Header extends Component {
                 onBlur={this.props.handleInputBlur}
               ></NavSearch>
             </CSSTransition>
-            <span
-              className={focused ? 'focused iconfont' : 'iconfont'}
-            >
+            <span className={focused ? 'focused iconfont' : 'iconfont'}>
               &#xe60d;
             </span>
             {this.getListArea()}
@@ -68,19 +66,40 @@ class Header extends Component {
   }
 
   getListArea() {
-    const { focused, list } = this.props;
-    if (focused) {
+    const {
+      focused,
+      list,
+      page,
+      totalPage,
+      mouseIn,
+      handleMouseEnter,
+      handleMouseLeave,
+      handleChangePage
+    } = this.props;
+    const newList = list.toJS();
+    const pageList = [];
+
+    if(newList.length){
+      for (let i = (page - 1) * 10; i < page * 10; i++) {
+        pageList.push(
+          <SearchInfoItem key={newList[i]}>{newList[i]}</SearchInfoItem>
+        );
+      }
+    }
+
+    if (focused || mouseIn) {
       return (
-        <SearchInfo>
+        <SearchInfo
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
           <SearchInfoTitle>
             热门搜索
-            <SearchInfoSwitch>换一批</SearchInfoSwitch>
+            <SearchInfoSwitch onClick={() => handleChangePage(page, totalPage)}>
+              换一批
+            </SearchInfoSwitch>
           </SearchInfoTitle>
-          <SearchInfoList>
-            {list.map(item => {
-              return <SearchInfoItem>{item}</SearchInfoItem>;
-            })}
-          </SearchInfoList>
+          <SearchInfoList>{pageList}</SearchInfoList>
         </SearchInfo>
       );
     } else {
@@ -92,7 +111,10 @@ class Header extends Component {
 const mapStateToProps = state => {
   return {
     focused: state.getIn(['header', 'focused']),
-    list: state.getIn(['header', 'list'])
+    list: state.getIn(['header', 'list']),
+    page: state.getIn(['header', 'page']),
+    mouseIn: state.getIn(['header', 'mouseIn']),
+    totalPage: state.getIn(['header', 'totalPage'])
   };
 };
 const mapDispatchToProps = dispatch => {
@@ -103,6 +125,19 @@ const mapDispatchToProps = dispatch => {
     },
     handleInputBlur() {
       dispatch(actionCreators.searchBlur());
+    },
+    handleMouseEnter() {
+      dispatch(actionCreators.mouseEnter());
+    },
+    handleMouseLeave() {
+      dispatch(actionCreators.mouseLeave());
+    },
+    handleChangePage(page, totalPage) {
+      if(page<totalPage){
+        dispatch(actionCreators.changePage(page+1));
+      }else{
+        dispatch(actionCreators.changePage(1));
+      } 
     }
   };
 };
--- src/common/header/store/actionCreators.js
+++ src/common/header/store/actionCreators.js
@@ -11,22 +11,36 @@ export const searchBlur = () => ({
   type: constants.SEARCH_BLUR
 });
 
+export const mouseEnter = () => ({
+  type: constants.MOUSE_ENTER
+});
+
+export const mouseLeave = () => ({
+  type: constants.MOUSE_LEAVE
+});
+
+export const changePage = (page) =>({
+  type:constants.CHANGE_PAGE,
+  page
+})
+
 export const getList = () => {
   return dispatch => {
     // console.log(store.getIn(['header','list']))
     axios
-    .get('/api/headerList.json')
-    .then(res => {
-      const data = res.data;
-      dispatch(changeList(data.data));
-    })
-    .catch(() => {
-      console.log('error');
-    });
+      .get('/api/headerList.json')
+      .then(res => {
+        const data = res.data;
+        dispatch(changeList(data.data));
+      })
+      .catch(() => {
+        console.log('error');
+      });
   };
 };
 
 const changeList = data => ({
   type: constants.CHANGE_LIST,
-  data: fromJS(data)
+  data: fromJS(data),
+  totalPage: Math.ceil(data.length / 10)
 });
