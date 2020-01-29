adafd143f59f541a533d6e1a6a0b125ca5a0594c
Sniper <linqifujian@foxmail.com>
2020-01-29 21:20:39 +0800

    首页列表页功能开发

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -14,6 +14,30 @@ const defaultState = fromJS({
       imgUrl:
         '//upload-images.jianshu.io/upload_images/10986034-48d3c17c441ecdb2?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
     }
+  ],
+  articleList:[{
+    id:1,
+    title:'猫主子偷吻大橘得逞，橘猫懵：你好坏哦，不知羞！网友：我酸了',
+    desc:'编者按：所谓生活本该多姿多彩，充满情调的日子才叫生活！喵星人们也是深谙此道啊，难怪铲屎官们总喜欢给猫咪找个伴儿，一起成长，果然是身心愉悦、有利健...',
+    imgUrl:'//upload-images.jianshu.io/upload_images/14352811-c895351d341899be.jpeg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+  },{
+    id:2,
+    title:'猫主子偷吻大橘得逞，橘猫懵：你好坏哦，不知羞！网友：我酸了',
+    desc:'编者按：所谓生活本该多姿多彩，充满情调的日子才叫生活！喵星人们也是深谙此道啊，难怪铲屎官们总喜欢给猫咪找个伴儿，一起成长，果然是身心愉悦、有利健...',
+    imgUrl:'//upload-images.jianshu.io/upload_images/14352811-c895351d341899be.jpeg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+  },
+  {
+    id:3,
+    title:'猫主子偷吻大橘得逞，橘猫懵：你好坏哦，不知羞！网友：我酸了',
+    desc:'编者按：所谓生活本该多姿多彩，充满情调的日子才叫生活！喵星人们也是深谙此道啊，难怪铲屎官们总喜欢给猫咪找个伴儿，一起成长，果然是身心愉悦、有利健...',
+    imgUrl:'//upload-images.jianshu.io/upload_images/14352811-c895351d341899be.jpeg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+  },
+  {
+    id:4,
+    title:'猫主子偷吻大橘得逞，橘猫懵：你好坏哦，不知羞！网友：我酸了',
+    desc:'编者按：所谓生活本该多姿多彩，充满情调的日子才叫生活！喵星人们也是深谙此道啊，难怪铲屎官们总喜欢给猫咪找个伴儿，一起成长，果然是身心愉悦、有利健...',
+    imgUrl:'//upload-images.jianshu.io/upload_images/14352811-c895351d341899be.jpeg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+  }
   ]
 });
 
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -21,26 +21,56 @@ export const HomeLeft = styled.div`
 export const TopicWrapper = styled.div`
   overflow: hidden;
   padding: 20px 0 10px 0;
-  margin-left:-18px;
+  margin-left: -18px;
+  border-bottom: 1px solid #dcdcdc;
 `;
 export const TopicItem = styled.div`
   float: left;
   height: 32px;
   line-height: 32px;
-  margin-left:18px;
-  margin-bottom:18px;
-  padding-right:10px;
-  font-size: 14px; 
+  margin-left: 18px;
+  margin-bottom: 18px;
+  padding-right: 10px;
+  font-size: 14px;
   color: #000;
-  border:1px solid #dcdcdc;
-  border-radius:4px;
+  border: 1px solid #dcdcdc;
+  border-radius: 4px;
   background: #f7f7f7;
   .topic-pic {
-    display:block;
-    float:left;
-    margin-right:10px;
-    height:32px;
-    width:32px;
+    display: block;
+    float: left;
+    margin-right: 10px;
+    height: 32px;
+    width: 32px;
+  }
+`;
+
+export const ListInfo = styled.div`
+  width: 500px;
+  float: left;
+  .title {
+    line-height: 27px;
+    font-size: 18px;
+    font-weight: bold;
+    color: #333;
+  }
+  .desc {
+    line-height: 24px;
+    font-size: 13px;
+    color: #999;
+  }
+`;
+
+export const ListItem = styled.div`
+  overflow: hidden;
+  padding: 20px 0;
+  border-bottom: 1px solid #dcdcdc;
+  .pic {
+    display: block;
+    width: 125px;
+    height: 100px;
+    float: right;
+    border-radius: 10px;
   }
 `;
 
