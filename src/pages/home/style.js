4a7166991f3354d6e6f3b704e44f9146dba5ac96
Sniper <linqifujian@foxmail.com>
2020-01-29 17:45:08 +0800

    添加首页布局

---
+++ src/pages/home/style.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const HomeWrapper = styled.div`
+    overflow:hidden;
+    width:960px;
+    /* height:600px; */
+    margin:0 auto;
+`;
+
+export const HomeLeft = styled.div`
+    float:left;
+    margin-left:15px;
+    padding-top:30px;
+    width:625px;
+    .banner-img{
+        width:625px;
+        height:270px;
+    }
+`;
+
+export const HomeRight = styled.div`
+    width:240px;
+    float:right;
+`;
+
+
+
