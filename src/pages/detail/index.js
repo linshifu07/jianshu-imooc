7d845cf7d7bdd7488dcbaaefb0f39def0f56cf90
Sniper <linqifujian@foxmail.com>
2020-01-31 15:38:21 +0800

    简书详情页开发

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,10 +1,34 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react';
+import { Detailwrapper, Header, Content } from './style';
+import { connect } from 'react-redux';
+import { actionCreators } from './store';
 
 class Detail extends Component {
-    render(){
-        return(
-            <div>Detail~</div>
-        )
-    }
+  componentDidMount() {
+    this.props.getDetail();
+  }
+
+  render() {
+    const { title, content } = this.props;
+    return (
+      <Detailwrapper>
+        <Header>{title}</Header>
+        <Content dangerouslySetInnerHTML={{ __html: content }} />
+        <Content></Content>
+      </Detailwrapper>
+    );
+  }
 }
-export default Detail
+
+const mapState = state => ({
+  title: state.getIn(['detail', 'title']),
+  content: state.getIn(['detail', 'content'])
+});
+
+const mapDispatch = dispatch => ({
+  getDetail() {
+    dispatch(actionCreators.getDetail());
+  }
+});
+
+export default connect(mapState, mapDispatch)(Detail);
